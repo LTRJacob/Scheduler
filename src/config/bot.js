f58a42409d6fc30d00c259a77665f0dafe9c4c4f
LTRJacob <jacobyaffe39@gmail.com>
2026-07-26 16:10:49 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "Join the Knockout Cup Today!",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
